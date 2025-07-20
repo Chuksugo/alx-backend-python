046a235155bce33e0e31a7d820b801f0d76f1c3f
Chuksugo <rhemekas.inc@gmail.com>
2025-07-20 17:06:34 +0000

    Parameterize Exception Handling

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test normal access of nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test KeyError is raised with bad paths"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), repr(path[-1]))
+
