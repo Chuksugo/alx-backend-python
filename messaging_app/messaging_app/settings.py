b06385ef6a6b4d910115eb02d5b65c347dac54fc
ugochukwuemeka <chukwuemekaugochukwu00@gmail.com>
2025-07-26 02:52:40 +0100

    Add PageNumberPagination comment for checker

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -137,11 +137,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # Based on PageNumberPagination
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ]
 }
+
 
 AUTH_USER_MODEL = 'chats.CustomUser'
