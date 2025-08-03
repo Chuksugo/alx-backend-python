d737ec3b6ef72930469fe1cede39a41f97b5b99b
ugochukwuemeka <chukwuemekaugochukwu00@gmail.com>
2025-08-03 23:31:45 +0100

    Fix missing parenthesis in messaging app config

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,16 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message, Notification
+from django.db.models.signals import post_delete
+from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
-
-
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from .models import Message, MessageHistory
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
@@ -26,3 +26,15 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete messages where user was sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete notifications for the user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message history edited by the user (if edited_by field is used)
+    MessageHistory.objects.filter(edited_by=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+@login_required
+def delete_user(request):
+    if request.method == "POST":
+        user = request.user
+        user.delete()
+        return redirect('home')  # or your login/home page
