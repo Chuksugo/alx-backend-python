f5210d966d54317f1d05d2f0ac270423b4e1061f
ugochukwuemeka <chukwuemekaugochukwu00@gmail.com>
2025-08-03 23:10:03 +0100

    Add MessageHistory model and pre_save signal for logging message edits

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -19,3 +19,23 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} about message {self.message.id}"
+    
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)  # NEW
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver} at {self.timestamp}"
+    
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey('Message', on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of message #{self.message.id} edited at {self.edited_at.strftime('%Y-%m-%d %H:%M:%S')}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -6,3 +6,22 @@ from .models import Message, Notification
 def create_notification_on_message(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Message, MessageHistory
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_instance = Message.objects.get(pk=instance.pk)
+            if old_instance.content != instance.content:
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_instance.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
